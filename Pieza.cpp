600df98d527ffb2ffcb8b0897f96cddc5d7fada1
CJAlvarez <cristhianalvarez96@gmail.com>
2017-03-10 13:57:52 -0600

    Saving files

--- Pieza.cpp
+++ Pieza.cpp
@@ -1,1 +1,17 @@
 // Pieza.cpp
+#include "Pieza.h"
+
+Pieza::Pieza() {
+	color = dentro = 0;
+}
+
+Pieza::Pieza(int color) {
+	this -> color = color;
+	dentro = 0;
+}
+
+int Pieza::getColor() {
+	return color;
+}
+
+int Pieza::EntrarTablero
--- Pieza.h
+++ Pieza.h
@@ -1,1 +1,21 @@
 // Pieza.h
+#pragma once
+
+#include <ncurses.h>
+
+class Pieza {
+	int color;
+	int dentro;
+	int final;
+
+	public: 
+		Pieza();
+		// @param color
+		Pieza(int);
+
+		int getColor();
+
+		int EntrarTablero();
+		int ZonaFinal();
+		int SalirTablero();
+};
