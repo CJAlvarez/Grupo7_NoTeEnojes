4a4a6f8c0a05a51b6995b9d94a508ce49b97c256
Arles96 <arles.cerrato@gmail.com>
2017-03-10 14:11:19 -0600

    Add code in PiezaAmarillo.h and PiezaVerde.h

--- PiezaVerde.h
+++ PiezaVerde.h
@@ -1,1 +1,20 @@
-// PiezaVerde.h
+#ifndef PIEZAVERDE_H_
+#define PIEZAVERDE_H_
+
+#include "Pieza.h"
+
+class PiezaVerde : public Pieza
+{
+public:
+
+  //Constructores
+  PiezaVerde();
+  PiezaVerde(int);
+
+  //funciones polimorficas
+  virtual int EntrarTablero();
+  virtual int ZonaFinal();
+  virtual int SalirTablero();
+};
+
+#endif
